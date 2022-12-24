e58f6f501673ded48fbc07cb774bfe8fc09c84ca
Karlos P <charly@tripleboot.de>
2022-12-23 21:21:52 -0600

    cambios en el layout

--- wwwroot/scripts/out.js
+++
@@ -1,8 +0,0 @@
-
-window.PlayAudio = (elementName) => {
-  document.getElementById(elementName).play();
-}
-
-window.PauseAudio = (elementName) => {
-  document.getElementById(elementName).pause();
-}
